a63eaf3ef7d49ac21d2c4f47e6c56ce35cb4dc40
Sammas21 <sam1992.123@gmail.com>
2018-06-22 12:01:30 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { MainAppComponent } from './main-app/main-app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    MainAppComponent
   ],
   imports: [
     BrowserModule
